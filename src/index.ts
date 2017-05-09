dd3c6aba23e304a8483a3ca204da121eadb6dbd2
Sebastien Chopin <sebastien.chopin@neo9.fr>
2017-05-09 12:58:28 +0200

    base: Remove awaiting

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 // Add source map supports
 import 'source-map-support/register'
 
-// NPM modules
-import * as a from 'awaiting'
-
 // Load project conf & set as global
 import config from './modules/conf'
 global.conf = config
 
 // Provides a stack trace for unhandled rejections instead of the default message string.
-// See https://hunterloftis.github.io/awaiting/#throw
-a.throw()
+process.on('unhandledRejection', (err) => {
+	throw err
+})
 
 // Load modules
 import './modules/newrelic'
